d0e96eb87a3d903bebf2d3afec2cb76f9437e277
Frederick Coleman <frc217@gmail.com>
2018-01-23 14:51:58 -0500

    updates to index

--- js/game.js
+++ js/game.js
@@ -21,5 +21,44 @@ function round(user_str) {
 	};
 
 	userPlay.innerHTML = "You chose "+user_str+".";
-	var comp_str = computer_play();
+	let comp_str = computer_play();
 	compPlay.innerHTML = "The computer chose "+comp_str+".";
+}
+
+let computer_score = 0;
+let user_score = 0;
+let result = [];
+
+buttons.forEach((button) => {
+	button.addEventListener("click", function() {
+		roundResult.innerHTML = " ";
+		if (button.id == 'rock')
+			result = round('rock');
+		else if (button.id == 'paper')
+			result = round('paper');
+		else if (button.id == 'scissors')
+			result = round('scissors');
+			
+		if (result.indexOf("Lose") != -1) {
+			computer_score += 1;
+		}
+		else if (result.indexOf("Win") != -1) {
+			user_score += 1;
+		};
+		compScore.innerHTML = computer_score;
+		userScore.innerHTML = user_score;
+		if (user_score >= 5) {
+			roundResult.innerHTML = "Congrats chale! You won the game!";
+			computer_score = 0;
+			user_score = 0;
+		}
+		else if (computer_score >= 5) {
+			roundResult.innerHTML = "Sorry chale! You lost the game!";
+			computer_score = 0;
+			user_score = 0;
+		}
+	});
+});
+
+
+
